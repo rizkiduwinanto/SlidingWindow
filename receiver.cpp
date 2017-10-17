d9661736c8cb2200efe38cb9fb9ca15f8cc8c46b
audrynyonata <audrynyonata@gmail.com>
2017-10-17 15:46:16 +0700

    ketuker

--- receiver.cpp
+++ receiver.cpp
@@ -15,41 +15,39 @@ struct sockaddr_storage serverStorage;
 socklen_t addressSize;
 
 int main(int argc, char* argv[]) {
-  if(argc != 6){
-    cout << "Usage : ./receiver <filename> <windowsize> <buffersize> <destination_ip> <destination_port>";
+  if (argc != 5) {
+    cout << "Usage : ./receiver​ <filename>​ ​ <windowsize>​ ​ <buffersize>​ ​ <port>";
   } else {
-    //INPUT dari SendFile
+    //INPUT dari recvfile
     char* fileName = argv[1];
     int windowSize = atoi(argv[2]);
     int bufferSize = atoi(argv[3]);
-    char* IP = argv[4];
-    int portNumber = atoi(argv[5]);
-    int clientSocket;
-	
-	fileName = "textfile.txt";
-    windowSize = 1;
-    bufferSize = 256;
-    IP = "127.0.0.1";
-    portNumber = 7891;
-	
-	char buffer[256];
-	
-	// UDP
-	clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    int portNumber = atoi(argv[4]);
+    int udpSocket;
+    char buffer[bufferSize];
+    
+    //UDP socket
+    if ((udpSocket = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		cout << "ERROR : cannot create socket" << endl;
+		return 0;
+	}
 	serverAddress.sin_family = AF_INET;
 	serverAddress.sin_port = htons(portNumber);
-	serverAddress.sin_addr.s_addr = inet_addr(IP);
-	memset(serverAddress.sin_zero, '\0', sizeof serverAddress.sin_zero);  
-	
-	addressSize = sizeof serverAddress;
-	
-	while(1) {
-		buffer[0] = 'h';
-		printf("Send to server: %s\n",buffer);
-		int nBytes = strlen(buffer) + 1;
-	    sendto(clientSocket,buffer,nBytes,0,(struct sockaddr *)&serverAddress,addressSize);
-		nBytes = recvfrom(clientSocket,buffer,bufferSize,0,NULL, NULL);
-		printf("Received from server: %s\n",buffer);
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+    memset(serverAddress.sin_zero, '\0', sizeof serverAddress.sin_zero);
+    
+    
+    //BIND
+    if (bind(udpSocket,(struct sockaddr*) &serverAddress, sizeof(serverAddress))) {
+		cout << "ERROR : bind failed" << endl;
+		return 0;
+	}
+    
+    addressSize = sizeof serverStorage;
+    
+    while(1) {
+	int nBytes = recvfrom(udpSocket,buffer,bufferSize,0,(struct sockaddr *)&serverStorage, &addressSize);
+	sendto(udpSocket,buffer,nBytes,0,(struct sockaddr *)&serverStorage,addressSize);
 	}
   }
   return 0;
--- sender.cpp
+++ sender.cpp
@@ -15,37 +15,49 @@ struct sockaddr_storage serverStorage;
 socklen_t addressSize;
 
 int main(int argc, char* argv[]) {
-  if (argc != 5) {
-    cout << "Usage : ./sender​ ​ <filename>​ ​ <windowsize>​ ​ <buffersize>​ ​ <port>";
+  if(argc != 6){
+    cout << "Usage : ./sender <filename> <windowsize> <buffersize> <destination_ip> <destination_port>";
   } else {
-    //INPUT dari recvfile
+    //INPUT dari SendFile
     char* fileName = argv[1];
     int windowSize = atoi(argv[2]);
     int bufferSize = atoi(argv[3]);
-    int portNumber = atoi(argv[4]);
-    int udpSocket;
-    
-    fileName="textfile.txt";
-    windowSize = 1;
-    bufferSize = 256;
-    portNumber = 7891;
-    
-    char buffer[bufferSize];
-    
-    //UDP socket
-    udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
-    serverAddress.sin_family = AF_INET;
+    char* IP = argv[4];
+    int portNumber = atoi(argv[5]);
+    int clientSocket;
+	
+	char buffer[bufferSize];
+	
+	// UDP
+	clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
+	serverAddress.sin_family = AF_INET;
 	serverAddress.sin_port = htons(portNumber);
-    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
-    memset(serverAddress.sin_zero, '\0', sizeof serverAddress.sin_zero);
-    
-    bind(udpSocket,(struct sockaddr*) &serverAddress, sizeof(serverAddress));
-    
-    addressSize = sizeof serverStorage;
-    
-    while(1) {
-	int nBytes = recvfrom(udpSocket,buffer,bufferSize,0,(struct sockaddr *)&serverStorage, &addressSize);
-	sendto(udpSocket,buffer,nBytes,0,(struct sockaddr *)&serverStorage,addressSize);
+	serverAddress.sin_addr.s_addr = inet_addr(IP);
+	memset(serverAddress.sin_zero, '\0', sizeof serverAddress.sin_zero);  
+	
+	addressSize = sizeof serverAddress;
+	
+	while(1) {
+		/** TEMPORARY BUFFER **/
+		FILE *fp;
+		int c;
+		int x = 0;
+		fp = fopen(fileName,"r");
+		if (fp == NULL) {
+			cout << "ERROR : file not found" << endl;
+			return 0;
+		}
+		while (x<bufferSize && (c = fgetc(fp)) != EOF) {
+		  buffer[x] = c ;
+		  x++;
+		}
+		if (feof(fp)){ fclose(fp);}
+		
+		cout << "Send to server: "<< buffer << endl;
+		int nBytes = strlen(buffer) + 1;
+	    sendto(clientSocket,buffer,nBytes,0,(struct sockaddr *)&serverAddress,addressSize);
+		nBytes = recvfrom(clientSocket,buffer,bufferSize,0,NULL, NULL);
+		cout << "Received from server: "<< buffer << endl;	
 	}
   }
   return 0;
